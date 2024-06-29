be2b099be8fc5ec1945e83e946b5da083f284b77
Dumitriu Oana-Florentina <121244115+Oana-Florentina@users.noreply.github.com>
2024-06-29 15:49:33 +0200

    Post Repo

---
+++ Lunatic.Application/Persistence/IPostRepository.cs
@@ -0,0 +1,16 @@
+﻿using Lunatic.Domain.Entities;
+using Lunatic.Domain.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lunatic.Application.Persistence
+{
+    public interface IPostRepository : IAsyncRepository<Post>
+    {
+        Task<Result<List<Post>>> GetPostsByBookClubIdAsync(Guid bookClubId);
+
+    }
+}
---
+++ Lunatic.Infrastructure/Repositories/Post/PostRepository.cs
@@ -0,0 +1,37 @@
+﻿using Lunatic.Application.Persistence;
+using Lunatic.Domain.Entities;
+using Lunatic.Domain.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lunatic.Infrastructure.Repositories
+{
+    public class PostRepository : RepositoryBase<Post>, IPostRepository
+    {
+        public PostRepository(LunaticContext context) : base(context)
+        {
+        }
+
+        public async Task<Result<List<Post>>> GetPostsByBookClubIdAsync(Guid bookClubId)
+        {
+            var result = await GetAllAsync();
+            if (result.IsSuccess)
+            {
+                List<Post> posts = result.Value.ToList();
+                List<Post> requestsForBookClub = posts
+                    .Where(fr => fr.BookClubId == bookClubId)
+                    .ToList();
+
+                if (!requestsForBookClub.Any())
+                {
+                    return Result<List<Post>>.Failure($"No friend requests found for BookClub ID {bookClubId}");
+                }
+                return Result<List<Post>>.Success(requestsForBookClub);
+            }
+            return Result<List<Post>>.Failure($"Error retrieving friend requests for BookClub ID {bookClubId}");
+        }
+    }
+}
