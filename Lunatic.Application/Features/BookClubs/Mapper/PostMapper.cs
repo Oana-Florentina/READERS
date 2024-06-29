47fc936d605896a4c1cf212c2607deb0f98b4243
Dumitriu Oana-Florentina <121244115+Oana-Florentina@users.noreply.github.com>
2024-06-29 16:25:18 +0200

    post mapper + post dto

---
+++ Lunatic.Application/Features/BookClubs/Mapper/PostMapper.cs
@@ -0,0 +1,27 @@
+﻿using Lunatic.Application.Features.BookClubs.Payload;
+using Lunatic.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lunatic.Application.Features.BookClubs.Mapper
+{
+    public class PostMapper
+    {
+
+        public static PostDto MapToPostDto(PostDto post)
+        {
+            return new PostDto
+            {
+                PostId = post.PostId,
+                UserId = post.UserId,
+                BookClubId = post.BookClubId,
+                CreatedDate = post.CreatedDate,
+                Text = post.Text
+
+            };
+        }
+    }
+}
---
+++ Lunatic.Application/Features/BookClubs/Payload/PostDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lunatic.Application.Features.BookClubs.Payload
+{
+    public class PostDto
+    {
+        public Guid PostId { get; set; } = default!;
+        public Guid UserId { get; set; } = default!;
+        public Guid BookClubId { get; set; } = default!;
+        public DateTime CreatedDate { get; set; } = default!;
+        public string Text { get; set; } = default!;
+
+    }
+}
